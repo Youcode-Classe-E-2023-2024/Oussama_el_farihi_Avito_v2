56156d0f20753df97e738605d77b9ec640fdf2de
oussamaef06 <oussamaelfarihi@gmail.com>
2023-11-24 23:26:11 +0100

    creat navDash file wher i put the dashboard navbar the include it in the other files

---
+++ navDash.php
@@ -0,0 +1,26 @@
+<div class="min-h-full">
+    <nav class="bg-gray-800">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+          <div class="flex items-center">
+            <a href="index.php" class="flex-shrink-0">
+              <img class="h-8 w-8" src="img/avito_logo.png" alt="Avito">
+            </a>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <a href="profile.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                  aria-current="page">Profile</a>
+                <a href="dashboard.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                  aria-current="page">Add</a>
+                <a href="delete.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                  aria-current="page">Delete</a>
+                <a href="edit.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                  aria-current="page">Edit</a>
+                  <a href="edit.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                  aria-current="page">Users</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
