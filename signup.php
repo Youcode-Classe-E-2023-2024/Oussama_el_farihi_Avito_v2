9fff712c3f295a283fe05d6a6082a33c62b0b480
oussamaef06 <oussamaelfarihi@gmail.com>
2023-11-24 23:13:51 +0100

    edit title

--- signup.php
+++ signup.php
@@ -34,7 +34,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>Sign up</title>
 </head>
 
 <body>
