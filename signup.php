a9a9400af145a471e0e99c9de72f678cf5b4ff5b
oussamaef06 <oussamaelfarihi@gmail.com>
2023-11-24 17:10:08 +0100

    creating sing up page

---
+++ signup.php
@@ -0,0 +1,97 @@
+<?php
+
+include('connection.php');
+
+if (isset($_POST['submit'])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $user_type = $_POST["user_type"];
+    $password = $_POST["password"];
+
+    $stmt = $conn->prepare("INSERT INTO `user` (name, email, user_type, password) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param('ssss', $name, $email, $user_type, $password);
+
+    if ($stmt->execute()) {
+        echo "<script>alert('Sign up done successfully');</script>";
+    } else {
+        echo "<script>alert('Failed to execute statement: " . $stmt->error . "');</script>";
+    }
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company">
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign Up</h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form class="space-y-6" method="POST" enctype="multipart/form-data">
+
+                <div>
+                    <label class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                    <div class="mt-2">
+                        <input id="name" for="name" name="name" type="text" autocomplete="name" required
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                    <div class="mt-2">
+                        <input id="email" for="email" name="email" type="email" autocomplete="email" required
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div>
+                    <label class="block text-sm font-medium leading-6 text-gray-900">User Type</label>
+                    <div class="mt-2">
+                        <select for="user_type" id="dropdown"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            onchange="toggleInputVisibility()">
+                            <option value="">Utilisatuer</option>
+                            <option value="">Annonceur</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <div class="mt-4 flex items-center justify-between">
+                            <label for="password"
+                                class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                            <div class="text-sm">
+                            </div>
+                        </div>
+                        <div class="mt-2">
+                            <input id="password" for="password" name="password" type="password" autocomplete="current-password"
+                                required
+                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit"
+                            class="mt-6 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+                            in</button>
+                    </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
