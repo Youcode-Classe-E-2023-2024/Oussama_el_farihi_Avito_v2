19fbf0bd101ecae1e3570a462d02246ba5a4799b
oussamaef06 <oussamaelfarihi@gmail.com>
2023-11-25 02:00:33 +0100

    modifie header

--- delete.php
+++ delete.php
@@ -53,7 +53,7 @@ $products = $result->fetch_all(MYSQLI_ASSOC);
     <main>
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Edit Products</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Delete Products</h1>
             </div>
         </header>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
