ceda7cfd1bf58d75721e00a2c2d00954f091cdf6
oussamaef06 <oussamaelfarihi@gmail.com>
2023-11-25 01:17:39 +0100

    include navbar

--- product.php
+++ product.php
@@ -9,6 +9,9 @@
     <title>Product</title>
 </head>
 <body>
+  <!---- navbar --->
+  <?php include 'navbar.php'; ?>
+  <!---- navbar --->
 <div class="bg-white">
     <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
       <h2 class="text-4xl font-bold tracking-tight text-gray-900 text-center">Our Products</h2>
