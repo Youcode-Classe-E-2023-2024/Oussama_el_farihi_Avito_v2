cfe01edc8a1f14dbb1afc5888b86db7a07aca0d6
oussamaef06 <oussamaelfarihi@gmail.com>
2023-11-28 07:21:47 +0100

    add buy now button

--- product.php
+++ product.php
@@ -2,17 +2,19 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Product</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Product</title>
 </head>
+
 <body>
   <!---- navbar --->
   <?php include 'navbar.php'; ?>
   <!---- navbar --->
-<div class="bg-white">
+  <div class="bg-white">
     <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
       <h2 class="text-4xl font-bold tracking-tight text-gray-900 text-center">Our Products</h2>
       <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
@@ -21,7 +23,7 @@
         $sql = "SELECT * FROM annonce";
         $result = mysqli_query($conn, $sql);
 
-        
+
         while ($row = mysqli_fetch_assoc($result)) {
           $product_id = $row['id'];
           $product_title = $row['titre'];
@@ -47,6 +49,9 @@
             </div>
             <p class='text-sm font-medium text-gray-900'>$$product_price</p>
           </div>
+          <button class='bg-blue-500 text-white px-4 py-2 rounded-md mt-2 hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800'>
+    Buy Now
+</button>
           </div>";
         }
         ?>
@@ -137,4 +142,5 @@
     </div>
   </section>
 </body>
+
 </html>
