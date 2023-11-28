9b9601f4814574b60eacbab32fa8592b6d9f8f12
oussamaef06 <oussamaelfarihi@gmail.com>
2023-11-28 07:05:46 +0100

    fix links

--- login.php
+++ login.php
@@ -58,8 +58,7 @@ if (isset($_POST['submit'])) {
 
   <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-10 w-auto" src="img/avito_logo.png"
-        alt="Your Company">
+      <a href="index.php" ><img class="mx-auto h-10 w-auto" src="img/avito_logo.png"></a>
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account
       </h2>
     </div>
